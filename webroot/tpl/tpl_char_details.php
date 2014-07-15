6d3b3c83da08787b69425ac7c5d5c92429424ed4
kiu <kiu@gmx.net>
2014-07-15 12:04:21 +0200

    added eveboard links

--- webroot/tpl/tpl_char_details.php
+++ webroot/tpl/tpl_char_details.php
@@ -21,7 +21,7 @@ $charNameUrl = urlencode($charName);
 	    <?php echo "<td><img src='http://image.eveonline.com/Character/" . $charId . "_128.jpg'></td>" ?>
 	    <td style="vertical-align:text-top;padding-left:10px;padding-top:0px;">
 	    <?php echo "<span style='font-size:250%;font-weight:bold;'>" . $charDisplay . "</span><br>"; ?>
-	    <?php echo "<strong>Profile:</strong> " . linkOrText($charName, "https://gate.eveonline.com/Profile/" . $charName, "EVE Gate") . ", " . linkOrText($charName, "http://evewho.com/pilot/" . $charNameUrl, "Eve Who") . "<br>"; ?>
+	    <?php echo "<strong>Profile:</strong> " . linkOrText($charName, "https://gate.eveonline.com/Profile/" . $charName, "EVE Gate") . ", " . linkOrText($charName, "http://evewho.com/pilot/" . $charNameUrl, "Eve Who") . ", " . linkOrText($charName, "http://eveboard.com/pilot/" . preg_replace("/[\ ]/", '_', $charName), "eveboard") . "<br>"; ?>
 	    <?php echo "<strong>Bazaar:</strong> " . linkOrText($charName, "https://forums.eveonline.com/default.aspx?g=search&postedby=" . $charNameUrl . "&forumID=277", "Posted By") . ", " . linkOrText($charName, "https://forums.eveonline.com/default.aspx?g=search&search=%22" . $charNameUrl . "%22&forumID=277", "Contains") . "<br>"; ?>
 	    <?php echo "<strong>Killboard:</strong> <a href='https://zkillboard.com/character/" . $charId . "/'>zKillboard</a>, <a href='http://eve-kill.net/?a=pilot_detail&plt_external_id=" . $charId . "'>EVE-Kill</a>" ?>
 	</td>
